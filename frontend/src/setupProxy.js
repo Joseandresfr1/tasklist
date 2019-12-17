137c6017353c71b4f660f45afa2a71577a87e2fb
Joseandresfr1 <joseandresfr1@gmail.com>
2019-12-17 15:44:01 -0400

    F004: activar para docker

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,6 +3,11 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
-    //app.use(proxy('/api/*', { target: 'http://backend:5000' }))
-    app.use(proxy('/api', { target: 'http://localhost:5000' }))
+    // PARA COORER LOCALMENTE
+
+  // app.use(proxy('/api/*', { target: 'http://backend:5000' }))
+
+    // PARA COORER CON DOCKER
+
+     app.use(proxy('/api', { target: 'http://localhost:5000' }))
 }
