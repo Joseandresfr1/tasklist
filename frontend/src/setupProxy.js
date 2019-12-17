19d04936f76434dd59252c9761084198f348d7ea
Joseandresfr1 <joseandresfr1@gmail.com>
2019-12-17 16:04:11 -0400

    master: fix proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -5,9 +5,9 @@ const proxy = require('http-proxy-middleware')
 module.exports = function(app) {
     // PARA COORER LOCALMENTE
 
-  // app.use(proxy('/api/*', { target: 'http://backend:5000' }))
+    app.use(proxy('/api', { target: 'http://backend:5000' }))
 
     // PARA COORER CON DOCKER
 
-     app.use(proxy('/api', { target: 'http://localhost:5000' }))
+    // app.use(proxy('/api', { target: 'http://localhost:5000' }))
 }
